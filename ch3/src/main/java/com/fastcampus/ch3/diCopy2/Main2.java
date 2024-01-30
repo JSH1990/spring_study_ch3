3fe5b5429adbd18dd355d813cb70ac6cb63acf86
JSH1990 <tmdgus21@naver.com>
2024-01-30 17:59:40 +0900

    주석

--- ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -16,13 +16,13 @@ class door extends Engine{}
 class AppContext{
     Map map; //객체저장소
 
-    AppContext() throws Exception {
+    AppContext() throws Exception { //객체저장소 클래스
         Properties p = new Properties();
         p.load(new FileReader("config.txt"));
 
-        map = new HashMap(p);
+        map = new HashMap(p); //객체들을 담을 map을 생성
 
-        for (Object key : map.keySet()){
+        for (Object key : map.keySet()){ //for문을 통해 파일의 키들을 이용해 객체생성후 저장소에 보관
             Class clazz = Class.forName((String)map.get(key));
             map.put(key, clazz.newInstance());
         }
@@ -34,8 +34,8 @@ class AppContext{
 }
 public class Main2 {
     public static void main(String[] args) throws Exception {
-        AppContext ac = new AppContext();
-        Car car = (Car) ac.getBean("car"); //car 객체 만듦.
+        AppContext ac = new AppContext(); //객체저장소 ac생성
+        Car car = (Car) ac.getBean("car"); //ac.getBean(키는 car) 호출해 저장소에 보관
         Engine engine = (Engine) ac.getBean("engine");
         System.out.println("car = " + car);
         System.out.println("engine = " + engine);
