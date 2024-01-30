c89aead32ad2c893bdeccfdcfe048251291516bf
JSH1990 <tmdgus21@naver.com>
2024-01-30 13:31:05 +0900

    주석

--- ch3/src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -1,4 +1,4 @@
-package com.fastcampus.ch3.d1Copy1;
+package com.fastcampus.ch3.diCopy1;
 
 import java.io.FileReader;
 import java.util.Properties;
@@ -17,11 +17,13 @@ public class Main1 {
     }
 
     static Object getObject(String key) throws Exception{
+        //config.txt를 읽어서 Properties에 저장
         Properties p = new Properties();
         p.load(new FileReader("config.txt"));
 
+        //클래스 객체(설계도)를 얻어서
         Class clazz = Class.forName(p.getProperty(key));
-        return clazz.newInstance();
+        return clazz.newInstance();//객체를 생성해서 반환
     }
 
     static Car getCar() throws Exception{
