e1ba0738d5cc4864e64e2616906e46b3a82b27fc
JSH1990 <tmdgus21@naver.com>
2024-01-31 17:51:24 +0900

    di 3,4

--- ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
@@ -1,11 +1,25 @@
-package com.fastcampus.ch3.diCopy2;
+package com.fastcampus.ch3.diCopy3;
 
+
+import com.google.common.reflect.ClassPath;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+
+import javax.annotation.Resource;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
+
+@Component class Car{ }
+@Component class SportsCar extends Car{}
+@Component class Truck extends Car{}
+
+class Engine{}
 
 class AppContext{
     Map map; //객체저장소
@@ -15,21 +29,49 @@ class AppContext{
         doComponentScan();
         }
 
-    private void doComponentScan(){
+    private void doComponentScan() {
+        try {
+            // 1. 패키지내의 클래스 목록을 가져온다.
+            // 2. 반복문으로 클래스를 하나씩 읽어와서 @Component이 붙어 있는지 확인
+            // 3. @Component가 붙어있으면 객체를 생성해서 map에 저장
+            ClassLoader classLoader = com.fastcampus.ch3.diCopy3.AppContext.class.getClassLoader();
+            ClassPath classPath = ClassPath.from(classLoader);
 
+            Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.fastcampus.ch3.diCopy3");
+            for (ClassPath.ClassInfo classInfo : set) {
+                Class clazz = classInfo.load();
+                Component component = (Component) clazz.getAnnotation(Component.class);
+                if (component != null) {
+                    String id = StringUtils.uncapitalize(classInfo.getSimpleName());
+                    map.put(id, clazz.newInstance());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     Object getBean(String key) {
         return map.get(key);
     } //key에 해당되는 객체를 가져온다.
+
+    Object getBean(Class clazz){
+        for(Object obj : map.values()){
+            if(clazz.isInstance(obj))
+                return obj;
+        }
+        return null;
+    }
 }
-public class Main2 {
+public class Main3 {
     public static void main(String[] args) throws Exception {
-        AppContext ac = new AppContext(); //객체저장소 ac생성
-        Car car = (Car) ac.getBean("car"); //ac.getBean(키는 car) 호출해 저장소에 보관, getBean은 객체를 가져온다.
+        AppContext ac = new AppContext();
+        Car car = (Car) ac.getBean("car"); //byName으로 객체를 검색
+        Car car2 = (Car) ac.getBean(Car.class); //byType으로 객체를 검색
         Engine engine = (Engine) ac.getBean("engine");
         System.out.println("car = " + car);
         System.out.println("engine = " + engine);
+
     }
 /*
 @component class car{}
